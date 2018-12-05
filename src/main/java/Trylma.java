8ec6638f54ab88bf6bb4a3f0955cb5b821a82eb5
Mykola <mykola.medynsky@gmail.com>
2018-12-05 14:04:43 +0100

    my branch

--- src/main/java/Trylma.java
+++ src/main/java/Trylma.java
@@ -1,7 +1,8 @@
 public class Trylma {
 
     public static void main(String[] args) {
-        System.out.printf("Hello, geys!");
+        System.out.println("Hello, geys!");
+        System.out.println("This is my branch, scrab");
     }
 
 }
